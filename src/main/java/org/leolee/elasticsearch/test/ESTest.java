8f514564e48696145c84a50de7bae9afbf647585
LeoLee <512240816@qq.com>
2021-06-07 21:52:02 +0800

    first commit

---
+++ src/main/java/org/leolee/elasticsearch/test/ESTest.java
@@ -0,0 +1,14 @@
+package org.leolee.elasticsearch.test;
+
+/**
+ * @ClassName ESTest
+ * @Description: TODO
+ * @Author LeoLee
+ * @Date 2021/6/7
+ * @Version V1.0
+ **/
+public class ESTest {
+
+    public static void main(String[] args) {
+    }
+}
